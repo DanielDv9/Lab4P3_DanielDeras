f6015e4ebe36ed62574ad857d3eabba5d1d5072f
DanielDv9 <danielderas2006@gmail.com>
2025-08-15 14:36:51 -0600

    Commit 2:00

--- Lab4P3_DanielDeras.cpp
+++ Lab4P3_DanielDeras.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 using namespace std;
 #include <cstring>
+void imprimir_mensaje(char* mensaje) {
+	for (int i = 0; i < strlen(mensaje); i++) {
+		cout << mensaje[i];
+	}
+}
 int main() {
 	int resp;
 	char* mensaje = new char[51];
+	char* mensaje_anterior = new char[51];
 	bool validar = false;
+	int tamano_casa;
 	do {
 		cout << "--Bienvenido--\n";
 		cout << "1) ejercicio 1\n";
 		cout << "2) ejercicio 2\n";
 		cout << "0) Salir\n";
 		cin >> resp;
+		cin.ignore();
 		switch (resp) {
 		case 1:
 			do
 			{
+				validar = false;
 				cout << "Ingrese el mensaje a encriptar(En mayusculas): \n";
 				cin.getline(mensaje, 51);
 				for (int i = 0; i < strlen(mensaje); i++) {
@@ -23,6 +32,37 @@ int main() {
 					}
 				}
 			} while (validar);
+			strcpy_s(mensaje_anterior, strlen(mensaje) + 1, mensaje);
+			for (int i = 0; i < strlen(mensaje); i++) {
+				if ((int)mensaje[i] == 65) {
+					mensaje[i] = mensaje[i] + 2;
+
+				}else if ((int)mensaje[i] == 90) {
+					mensaje[i] = mensaje[i] - 2;
+
+				}
+				else if ((int)mensaje[i]%2 == 0) {
+					mensaje[i] = mensaje[i] + 2;
+				}
+				else {
+					mensaje[i] = (int)mensaje[i] - 2;
+				}
+			}
+			cout << "La desencriptacion de ";
+			imprimir_mensaje(mensaje_anterior);
+			cout << " es: ";
+			imprimir_mensaje(mensaje);
+			cout << endl;
+			break;
+		case 2:
+			do
+			{
+				cout << "Ingrese el numero de casas que hay en la residencial (entre 5 y 15): ";
+				cin >> tamano_casa;
+				if (tamano_casa < 5 || tamano_casa > 15) {
+					cout << "NUMERO INVALIDO\n";
+				}
+			} while (tamano_casa < 5 || tamano_casa > 15);
 			break;
 		default:
 			break;
