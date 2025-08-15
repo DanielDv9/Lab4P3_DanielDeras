37b57c88efa03161e737bfc4e5377f0659e5a253
DanielDv9 <danielderas2006@gmail.com>
2025-08-15 15:10:30 -0600

    Commit 3:00

--- Lab4P3_DanielDeras.cpp
+++ Lab4P3_DanielDeras.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
 using namespace std;
 #include <cstring>
+#include <cstdlib>
+#include <ctime>   
 void imprimir_mensaje(char* mensaje) {
 	for (int i = 0; i < strlen(mensaje); i++) {
 		cout << mensaje[i];
 	}
 }
+void liberar_memoria(char* mensaje) {
+	for (int i = 0; i < strlen(mensaje); i++) {
+		delete[] mensaje;
+	}
+}
+void combinacion_casas(int* arreglo, int& tamano_casa) {
+	int* arreglo_ganancia = new int[tamano_casa];
+	int posicion = 0;
+	int acum = 0;
+	for (int i = 0; i < tamano_casa; i++) {
+		for (int j = 0; j < tamano_casa; j++) {
+			if ((j + 2) > tamano_casa) {
+
+			} else {
+				acum += arreglo[j + 2];
+			}
+		}
+		arreglo_ganancia[i] = acum;
+		acum = 0;
+	}
+	cout << endl;
+	for (int i = 0; i < tamano_casa; i++) {
+		cout << " [" << arreglo_ganancia[i] << "] ";
+	}
+}
 int main() {
+	srand(time(0));
 	int resp;
 	char* mensaje = new char[51];
 	char* mensaje_anterior = new char[51];
 	bool validar = false;
 	int tamano_casa;
+	int* arreglo = NULL;
 	do {
 		cout << "--Bienvenido--\n";
 		cout << "1) ejercicio 1\n";
@@ -53,6 +82,7 @@ int main() {
 			cout << " es: ";
 			imprimir_mensaje(mensaje);
 			cout << endl;
+			liberar_memoria(mensaje);
 			break;
 		case 2:
 			do
@@ -63,6 +93,13 @@ int main() {
 					cout << "NUMERO INVALIDO\n";
 				}
 			} while (tamano_casa < 5 || tamano_casa > 15);
+			arreglo = new int[tamano_casa];
+			for (int i = 0; i < tamano_casa; i++) {
+				int numero_random = 500 + rand() % (10000 - 500 + 1);
+				arreglo[i] = numero_random;
+				cout << "Casa " << i << ": $" << arreglo[i] << endl;
+			}
+			combinacion_casas(arreglo, tamano_casa);
 			break;
 		default:
 			break;
